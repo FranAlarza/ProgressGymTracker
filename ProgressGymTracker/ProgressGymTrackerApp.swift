15c8903eeffc22522d18cd80cfc5abed80a1b083
Fran Alarza <franalarza@gmail.com>
2024-11-20 20:35:43 +0100

    first commit

---
+++ ProgressGymTracker/ProgressGymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressGymTrackerApp.swift
+//  ProgressGymTracker
+//
+//  Created by Fran Alarza on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressGymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProgressGymTrackerTests/ProgressGymTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressGymTrackerTests.swift
+//  ProgressGymTrackerTests
+//
+//  Created by Fran Alarza on 20/11/24.
+//
+
+import Testing
+@testable import ProgressGymTracker
+
+struct ProgressGymTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
